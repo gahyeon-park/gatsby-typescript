3d8c7ae8911b73cbda6fcfbfef3fc51cbbab55e4
gahyeon <pgh2031@inpix.com>
2023-09-18 17:22:39 +0900

    #10.13 Blog Post

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,10 +1,37 @@
 import React from "react";
 import Layout from "../../components/layout";
+import { graphql } from "gatsby";
+import Seo from "../../components/seo";
 
-export default function BlogPost() {
+interface IBlogPost {
+  data: Queries.PostDetailQuery;
+  children: any;
+}
+
+export default function BlogPost({ data, children }: IBlogPost) {
+  console.log(data, children);
+  
   return (
-    <Layout title="Blog Post">
-      <div></div>
+    <Layout title="">
+      <div>{children}</div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query PostDetail($frontmatter__slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter__slug }}) {
+      id
+      body
+      frontmatter {
+        author
+        category
+        date
+        title
+        slug
+      }
+    }
+  }
+`;
+
+export const Head = ({ data }: IBlogPost) => <Seo title={data.mdx?.frontmatter?.title!} />
