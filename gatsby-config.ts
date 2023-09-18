b0e798ae68a53955568fc613a0dedb6bc3233485
gahyeon <pgh2031@inpix.com>
2023-09-18 15:07:55 +0900

    #10.9 UseStateQuery

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,12 +2,14 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
+    title: `DevStickers`,
     siteUrl: `https://www.yourdomain.tld`,
+    description: `Bye the best stickers`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
+  graphqlTypegen: true, // → Gatsby가 우리의 쿼리를 읽고 자동으로 맞는 타입을 생성
   plugins: [],
 }
 
