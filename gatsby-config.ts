d6ed5b5cdc3e052f83c9b562ffa91c0ab78df739
gahyeon <pgh2031@inpix.com>
2023-09-18 15:34:00 +0900

    #10.10 Page Query

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,7 +10,14 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true, // → Gatsby가 우리의 쿼리를 읽고 자동으로 맞는 타입을 생성
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog-posts`,
+      }
+    }
+  ],
 }
 
 export default config
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
+import { graphql, PageProps } from "gatsby";
 
-function Blog() {
+function Blog({ data }: PageProps<Queries.BlogTitlesQuery>) {
+  console.log(data);
+  
   return (
     <Layout title="Hello welcom to my blog!">
-      <p> The most recent news from my shop. </p>
+      <ul>
+        {data.allFile.nodes.map((file, i) => <li key={i}>{file.name}</li>)}
+      </ul>
+      <p>The most recent news from my shop.</p>
     </Layout>
   );
 }
 
 export default Blog;
+
+export const query = graphql`
+  query BlogTitles {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="Blog" />;
